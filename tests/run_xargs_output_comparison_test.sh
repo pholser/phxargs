75eaf5b437d7350eab8cd8d4c6b5b83252ab5e47
Paul Holser <pholser@alumni.rice.edu>
2024-08-16 16:19:47 -0500

    write test output to its own dir

--- tests/run_xargs_output_comparison_test.sh
+++ tests/run_xargs_output_comparison_test.sh
@@ -1,17 +1,27 @@
 #!/usr/bin/env bash
 
-test_dir=$(dirname "$(readlink -f "$0")")
-cd "$test_dir" || exit 3
-
-build_dir="$test_dir/../build"
-
 test_name=$1
 xargs_input=$2
 xargs_options=$3
 cmd_line=$4
 
-xargs $xargs_options $cmd_line < "$xargs_input" > "xargs_$test_name.out" 2> "xargs_$test_name.err"
-"$build_dir/phxargs" $xargs_options $cmd_line < "$xargs_input"> "phxargs_$test_name.out" 2> "phxargs_$test_name.err"
+test_dir=$(dirname "$(readlink -f "$0")")
+cd "$test_dir" || exit 3
+
+build_dir="$test_dir/../build"
+test_output_dir="$build_dir/output"
+mkdir -p "$test_output_dir"
+
+xargs $xargs_options $cmd_line \
+  < "$xargs_input" \
+  > "$test_output_dir/xargs_$test_name.out" \
+  2> "$test_output_dir/xargs_$test_name.err"
+"$build_dir/phxargs" $xargs_options $cmd_line \
+  < "$xargs_input" \
+  > "$test_output_dir/phxargs_$test_name.out" \
+  2> "$test_output_dir/phxargs_$test_name.err"
+
+cd "$test_output_dir" || exit 3
 diff "xargs_$test_name.out" "phxargs_$test_name.out"
 out_comparison_failed=$?
 diff "xargs_$test_name.err" "phxargs_$test_name.err"
