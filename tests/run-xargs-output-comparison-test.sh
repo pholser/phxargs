bae5fbaadd6f4ac0f6dea0050420cff7f4eb1867
Paul Holser <pholser@alumni.rice.edu>
2024-08-16 16:19:48 -0500

    some renames in tests

--- tests/run-xargs-output-comparison-test.sh
+++ tests/run-xargs-output-comparison-test.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 test_name=$1
-xargs_input=$2
+xargs_input_producer_cmd=$2
 xargs_options=$3
-cmd_line=$4
+xargs_cmd_line=$4
 
 test_dir=$(dirname "$(readlink -f "$0")")
 cd "$test_dir" || exit 3
@@ -12,10 +12,12 @@ build_dir="$test_dir/../build"
 test_output_dir="$build_dir/output"
 mkdir -p "$test_output_dir"
 
-$xargs_input | xargs $xargs_options $cmd_line \
+$xargs_input_producer_cmd \
+  | xargs $xargs_options $xargs_cmd_line \
   > "$test_output_dir/xargs-$test_name.out" \
   2> "$test_output_dir/xargs-$test_name.err"
-$xargs_input | "$build_dir/phxargs" $xargs_options $cmd_line \
+$xargs_input_producer_cmd \
+  | "$build_dir/phxargs" $xargs_options $xargs_cmd_line \
   > "$test_output_dir/phxargs-$test_name.out" \
   2> "$test_output_dir/phxargs-$test_name.err"
 
