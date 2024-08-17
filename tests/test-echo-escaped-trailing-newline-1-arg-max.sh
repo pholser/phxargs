0da6748438c2acf5a12d3de9ec6e01578d5234b8
Paul Holser <pholser@alumni.rice.edu>
2024-08-16 22:26:42 -0500

    more tests

---
+++ tests/test-echo-escaped-trailing-newline-1-arg-max.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+source "$(dirname "$(readlink -f "$0")")"/set-test-context.sh
+
+cat > "$test_input" <<EOF
+a\\\nb c d
+EOF
+
+cat > "$expected_output" <<EOF
+a\\nb
+c
+d
+EOF
+
+./run-expected-output-comparison-test.sh \
+  $test_name \
+  "$test_input" \
+  "$expected_output" \
+  "$expected_err" \
+  '-n 1' \
+  ''
---
+++ tests/test-echo-max-lines-with-empty-lines.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+source "$(dirname "$(readlink -f "$0")")"/set-test-context.sh
+
+cat > "$test_input" <<EOF
+a
+
+b
+c
+
+d
+e
+  ff
+  
+
+EOF
+
+cat > "$expected_output" <<EOF
+a b
+c d
+e ff
+EOF
+
+./run-expected-output-comparison-test.sh \
+  $test_name \
+  "$test_input" \
+  "$expected_output" \
+  "$expected_err" \
+  '-L 2' \
+  ''
