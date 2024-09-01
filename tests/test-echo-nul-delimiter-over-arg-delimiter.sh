5f91d8ec822ff5aef07b35706493a9b7fddff156
Paul Holser <pholser@alumni.rice.edu>
2024-08-31 21:43:07 -0500

    more tests

---
+++ tests/test-echo-nul-delimiter-over-arg-delimiter.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+source "$(dirname "$(readlink -f "$0")")"/set-test-context.sh
+
+# Test conflicting options -0 and -d
+printf "arg1,arg2,arg3\0" > "$test_input"
+
+cat > "$expected_output" <<EOF
+arg1,arg2,arg3
+EOF
+
+./run-expected-output-comparison-test.sh \
+  $test_name \
+  "$test_input" \
+  "$expected_output" \
+  "$expected_error" \
+  '-d, -0' \
+  ''
