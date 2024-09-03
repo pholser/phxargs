d8d50db6cb76e1c27a4f194edd51b088d2aa9fb0
Paul Holser <pholser@alumni.rice.edu>
2024-09-02 19:07:44 -0500

    more tests

---
+++ tests/test-echo-line-mode-with-continuation-blanks.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+source "$(dirname "$(readlink -f "$0")")"/set-test-context.sh
+
+cat > "$test_input" <<EOF
+arg1 arg2 arg3\ \ \ \
+arg4 arg5 arg6\ \
+arg7 arg8 arg9\
+arg10 arg11
+EOF
+
+cat > "$expected_output" <<EOF
+arg1 arg2 arg3   arg4 arg5 arg6 arg7 arg8 arg9arg10 arg11
+EOF
+
+touch "$expected_error"
+
+./run-expected-output-comparison-test.sh \
+  $test_name \
+  "$test_input" \
+  "$expected_output" \
+  "$expected_error" \
+  '-L 2' \
+  ''
