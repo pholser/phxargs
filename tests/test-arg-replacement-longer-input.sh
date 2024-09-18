3a2a71b9a94f5a207811747525934f44b58c622c
Paul Holser <pholser@alumni.rice.edu>
2024-09-18 08:37:51 -0500

    more tests

---
+++ tests/test-arg-replacement-longer-input.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+source "$(dirname "$(readlink -f "$0")")"/set-test-context.sh
+
+# Long input with replacements
+cat > "$test_input" <<EOF
+$(printf "item%.0s\n" {1..100})
+EOF
+
+cat > "$expected_output" <<EOF
+$(printf "Processed item%.0s\n" {1..100})
+EOF
+
+./run-expected-output-comparison-test.sh \
+  $test_name \
+  "$test_input" \
+  "$expected_output" \
+  "$expected_error" \
+  '-I {} echo Processed {}' \
+  ''
