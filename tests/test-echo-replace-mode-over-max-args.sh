199c44d5793e498cb4b17b392722577115cbf71e
Paul Holser <pholser@alumni.rice.edu>
2024-09-19 23:26:59 -0500

    more tests. -n over -I failing

---
+++ tests/test-echo-replace-mode-over-max-args.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+source "$(dirname "$(readlink -f "$0")")"/set-test-context.sh
+
+cat > "$test_input" <<EOF
+arg1 arg2  arg3
+arg4  arg5 arg6
+EOF
+
+cat > "$expected_output" <<EOF
+boo arg1 arg2  arg3
+boo arg4  arg5 arg6
+EOF
+
+touch "$expected_error"
+
+./run-expected-output-comparison-test.sh \
+  $test_name \
+  "$test_input" \
+  "$expected_output" \
+  "$expected_error" \
+  '-n 2 -I {} echo boo {}' \
+  ''
