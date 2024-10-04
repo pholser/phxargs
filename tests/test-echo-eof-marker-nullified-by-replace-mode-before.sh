93a72cc6bc0d117e6b28eb046bdd7a8fdee4cd46
Paul Holser <pholser@alumni.rice.edu>
2024-10-04 11:38:04 -0500

    more tests

---
+++ tests/test-echo-eof-marker-nullified-by-replace-mode-before.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+source "$(dirname "$(readlink -f "$0")")"/set-test-context.sh
+
+cat > "$test_input" <<EOF
+arg1 arg2 arg3
+____
+arg4 arg5
+EOF
+
+cat > "$expected_output" <<EOF
+hello arg1 arg2 arg3
+hello ____
+hello arg4 arg5
+EOF
+
+cat > "$expected_error" <<EOF
+EOF
+
+./run-expected-output-comparison-test.sh \
+  $test_name \
+  "$test_input" \
+  "$expected_output" \
+  "$expected_error" \
+  '-I {} -E ___ echo hello {}' \
+  ''
