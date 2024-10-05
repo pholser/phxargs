fdfcb04f401b786688d1175a66d8dbf760746b50
Paul Holser <pholser@alumni.rice.edu>
2024-10-04 20:00:21 -0500

    more tests

---
+++ tests/test-echo-line-mode-with-blank-lines.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+source "$(dirname "$(readlink -f "$0")")"/set-test-context.sh
+
+cat > "$test_input" <<EOF
+arg1 arg2 arg3
+
+
+arg4 arg5 arg6
+
+
+
+
+arg7 arg8   arg9 arg10
+
+arg11 arg12
+EOF
+
+cat > "$expected_output" <<EOF
+arg1 arg2 arg3 arg4 arg5 arg6 arg7 arg8 arg9 arg10
+arg11 arg12
+EOF
+
+cat > "$expected_error" <<EOF
+EOF
+
+./run-expected-output-comparison-test.sh \
+  $test_name \
+  "$test_input" \
+  "$expected_output" \
+  "$expected_error" \
+  '-L 3' \
+  ''
