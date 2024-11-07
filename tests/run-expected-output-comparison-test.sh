5f4e98c9e84a5bf0307c2efed306b1508df5ee3b
Paul Holser <pholser@alumni.rice.edu>
2024-11-07 09:58:59 -0600

    wip re: expect tests

--- tests/run-expected-output-comparison-test.sh
+++ tests/run-expected-output-comparison-test.sh
@@ -15,24 +15,36 @@ phx_test_output_dir="$phx_build_dir/output"
 mkdir -p "$phx_test_output_dir"
 
 if [[ "$phx_xargs_options" =~ "-a"[[:space:]] ]]; then
-  input_flag=''
+  input_indicator=''
 else
-  input_flag="< $phx_test_input"
+  input_indicator="< $phx_test_input"
 fi
 
-if [[ "$phx_xargs_options" =~ "-a"[[:space:]] ]] ; then
-  /usr/bin/env -i "$phx_build_dir/phxargs" \
-    $phx_xargs_options \
-    $phx_xargs_cmd_line \
-    > "$phx_test_output_dir/phxargs-$phx_test_name.out" \
-    2> "$phx_test_output_dir/phxargs-$phx_test_name.err"
+if [[ "$phx_xargs_options" =~ "-p" ]] ; then
+  expect ./run-interactive-test-prompt.exp \
+    "$phx_test_name" \
+    "$phx_test_input" \
+    "$phx_expected_output" \
+    "$phx_expected_error" \
+    "$phx_xargs_options" \
+    "$phx_xargs_cmd_line \
+    $input_indicator
+elif [[ "$phx_args_options" =~ "-o" ]] ; then
+  expect ./run-interactive-test-open-tty.exp \
+    "$phx_test_name" \
+    "$phx_test_input" \
+    "$phx_expected_output" \
+    "$phx_expected_error" \
+    "$phx_xargs_options" \
+    "$phx_xargs_cmd_line \
+    $input_indicator
 else
   /usr/bin/env -i "$phx_build_dir/phxargs" \
     $phx_xargs_options \
     $phx_xargs_cmd_line \
-    < "$phx_test_input" \
     > "$phx_test_output_dir/phxargs-$phx_test_name.out" \
-    2> "$phx_test_output_dir/phxargs-$phx_test_name.err"
+    2> "$phx_test_output_dir/phxargs-$phx_test_name.err" \
+    $input_indicator
 fi
 
 cd "$phx_test_output_dir" || exit 3
